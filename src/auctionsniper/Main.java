e024fd6284d35bccdeb2474c0a287ebde714043e
tomykaira <tomykaira@gmail.com>
2011-09-05 14:18:52 +0900

    suppress all errors

---
+++ src/auctionsniper/Main.java
@@ -0,0 +1,17 @@
+package auctionsniper;
+
+public class Main {
+
+	public static final String MAIN_WINDOW_NAME = "Auction Sniper Main";
+	public static final String SNIPER_STATUS_NAME = "sniper status";
+	public static final String STATUS_JOINING = "Joining";
+	public static final String STATUS_LOST = "Lost";
+
+
+	public static void main(String xmppHostname, String sniperId,
+			String sniperPassword, String itemId) {
+		// TODO Auto-generated method stub
+		
+	}
+
+}
