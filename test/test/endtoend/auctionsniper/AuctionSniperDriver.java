43ee537fa895ee835c1970cdb4709930691197c6
tomykaira <tomykaira@gmail.com>
2011-09-05 14:32:04 +0900

    [from now] Mon Sep  5 14:32:04 JST 2011
    diff --git a/test/test/endtoend/auctionsniper/AuctionSniperDriver.java b/test/test/endtoend/auctionsniper/AuctionSniperDriver.java
    index b6295b6..6044f93 100644
    --- a/test/test/endtoend/auctionsniper/AuctionSniperDriver.java
    +++ b/test/test/endtoend/auctionsniper/AuctionSniperDriver.java
    @@ -12,7 +12,7 @@ public class AuctionSniperDriver extends JFrameDriver {
     	public AuctionSniperDriver(int timeoutMillis) {
     		super(new GesturePerformer(),
     				JFrameDriver.topLevelFrame(named(Main.MAIN_WINDOW_NAME), showingOnScreen()),
    -				new AWTEventQueueProber(timeoutMillis, 100));
    +					new AWTEventQueueProber(timeoutMillis, 100));
     	}
    
     	public void showsSniperStatus(String statusText) {

--- test/test/endtoend/auctionsniper/AuctionSniperDriver.java
+++ test/test/endtoend/auctionsniper/AuctionSniperDriver.java
@@ -12,7 +12,7 @@ public class AuctionSniperDriver extends JFrameDriver {
 	public AuctionSniperDriver(int timeoutMillis) {
 		super(new GesturePerformer(),
 				JFrameDriver.topLevelFrame(named(Main.MAIN_WINDOW_NAME), showingOnScreen()),
-				new AWTEventQueueProber(timeoutMillis, 100));
+					new AWTEventQueueProber(timeoutMillis, 100));
 	}
 	
 	public void showsSniperStatus(String statusText) {
