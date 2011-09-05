8c7eebb507cb2bfd039df98865fda4901418dcc4
tomykaira <tomykaira@gmail.com>
2011-09-05 13:26:40 +0900

    finish auction sniper driver

---
+++ test/test/endtoend/auctionsniper/AuctionSniperDriver.java
@@ -0,0 +1,20 @@
+package test.endtoend.auctionsniper;
+
+import static org.hamcrest.CoreMatchers.*;
+
+import com.objogate.wl.swing.AWTEventQueueProber;
+import com.objogate.wl.swing.driver.JFrameDriver;
+import com.objogate.wl.swing.driver.JLabelDriver;
+import com.objogate.wl.swing.gesture.GesturePerformer;
+
+public class AuctionSniperDriver extends JFrameDriver {
+	public AuctionSniperDriver(int timeoutMillis) {
+		super(new GesturePerformer(),
+				JFrameDriver.topLevelFrame(named(Main.MAIN_WINDOW_NAME), showingOnScreen()),
+				new AWTEventQueueProber(timeoutMillis, 100));
+	}
+	
+	public void showsSniperStatus(String statusText) {
+		new JLabelDriver(this, named(Main.SNIPER_STATUS_NAME)).hasText(equalTo(statusText));
+	}
+}
