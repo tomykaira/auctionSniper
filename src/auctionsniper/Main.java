5d807223057570c28c0d0818215c8f4938df18ef
tomykaira <tomykaira@gmail.com>
2011-09-05 14:56:01 +0900

    ui.MainWindow を作成して AuctionSniper のウィンドウを表示

--- src/auctionsniper/Main.java
+++ src/auctionsniper/Main.java
@@ -1,17 +1,34 @@
 package auctionsniper;
 
+import javax.swing.SwingUtilities;
+
+import auctionsniper.ui.MainWindow;
+
 public class Main {
 
-	public static final String MAIN_WINDOW_NAME = "Auction Sniper Main";
 	public static final String SNIPER_STATUS_NAME = "sniper status";
 	public static final String STATUS_JOINING = "Joining";
 	public static final String STATUS_LOST = "Lost";
+	
+	private MainWindow ui;
+	
+	public Main() throws Exception {
+		startUserInterface();
+	}
 
 
-	public static void main(String xmppHostname, String sniperId,
-			String sniperPassword, String itemId) {
-		// TODO Auto-generated method stub
+	private void startUserInterface() throws Exception {
+		SwingUtilities.invokeAndWait(new Runnable() {
+			public void run() {
+				ui = new MainWindow();
+			}
+		});
 		
+	}
+
+
+	public static void main(String ...args) throws Exception {
+		Main main = new Main();
 	}
 
 }
