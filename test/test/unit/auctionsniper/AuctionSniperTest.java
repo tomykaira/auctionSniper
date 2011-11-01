16f0ca24be7c9e4a1f8283946561e58f1345023d
tomykaira <tomykaira@gmail.com>
2011-11-02 08:22:12 +0900

    assert sniper state in AuctionSniperTest

--- test/test/unit/auctionsniper/AuctionSniperTest.java
+++ test/test/unit/auctionsniper/AuctionSniperTest.java
@@ -2,6 +2,7 @@ package test.unit.auctionsniper;
 
 import org.jmock.Expectations;
 import org.jmock.Mockery;
+import org.jmock.States;
 import org.jmock.integration.junit4.JMock;
 import org.junit.Test;
 import org.junit.runner.RunWith;
@@ -17,13 +18,28 @@ public class AuctionSniperTest {
 	private final Auction auction = context.mock(Auction.class);
 	private final SniperListener sniperListener = context.mock(SniperListener.class);
 	private final AuctionSniper sniper = new AuctionSniper(auction, sniperListener);
+	private final States sniperState = context.states("sniper");
 
 	@Test public void
-	reportsLostWhenAuctionClosed() {
+	reportsLostWhenAuctionClosedImmediately() {
 		context.checking(new Expectations() {{
 			one(sniperListener).sniperLost();
 		}});
 
+		sniper.auctionClosed();
+	}
+
+	@Test public void
+	reportsLostWhenAuctionClosedWhenBidding() {
+		context.checking(new Expectations() {{
+			ignoring(auction);
+			allowing(sniperListener).sniperBidding();
+				then(sniperState.is("bidding"));
+			atLeast(1).of(sniperListener).sniperLost();
+				when(sniperState.is("bidding"));
+		}});
+
+		sniper.currentPrice(123, 45, PriceSource.FromOtherBidder);
 		sniper.auctionClosed();
 	}
 
