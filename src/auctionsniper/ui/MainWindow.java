a3d63896215daceb35b3e836d7eadbcb78d70b82
tomykaira <tomykaira@gmail.com>
2011-11-02 09:10:50 +0900

    re-implement status with a single cell table

--- src/auctionsniper/ui/MainWindow.java
+++ src/auctionsniper/ui/MainWindow.java
@@ -1,14 +1,15 @@
 package auctionsniper.ui;
 
-import java.awt.Color;
+import java.awt.BorderLayout;
+import java.awt.Container;
 
 import javax.swing.JFrame;
-import javax.swing.JLabel;
-import javax.swing.border.LineBorder;
+import javax.swing.JScrollPane;
+import javax.swing.JTable;
 
 public class MainWindow extends JFrame {
 	public static final String MAIN_WINDOW_NAME = "Auction Sniper Main";
-	public static final String SNIPER_STATUS_NAME = "sniper status";
+	public static final String SNIPERS_TABLE_NAME = "Snieper Table";
 
 	public static final String STATUS_JOINING = "Joining";
 	public static final String STATUS_LOST = "Lost";
@@ -16,24 +17,29 @@ public class MainWindow extends JFrame {
 	public static final String	STATUS_WINNING	= "Winning";
 	public static final String	STATUS_WON	= "Won";
 
-	private final JLabel sniperStatus = createLabel(STATUS_JOINING);
+	private final SnipersTableModel snipers = new SnipersTableModel();
 
 	public MainWindow() {
 		super("Auction Sniper");
 		setName(MAIN_WINDOW_NAME);
-		add(sniperStatus);
+		fillContentPane(makeSnipersTable());
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		setVisible(true);
 	}
 
-	private JLabel createLabel(String initialText) {
-		JLabel result = new JLabel(initialText);
-		result.setName(SNIPER_STATUS_NAME);
-		result.setBorder(new LineBorder(Color.BLACK));
-		return result;
+	private void fillContentPane(JTable snipersTable) {
+		final Container contentPane = getContentPane();
+		contentPane.setLayout(new BorderLayout());
+		contentPane.add(new JScrollPane(snipersTable), BorderLayout.CENTER);
+	}
+
+	private JTable makeSnipersTable() {
+		final JTable snipersTable = new JTable(snipers);
+		snipersTable.setName(SNIPERS_TABLE_NAME);
+		return snipersTable;
 	}
 
 	public void showStatus(String status) {
-		sniperStatus.setText(status);
+		snipers.setStatusText(status);
 	}
 }
