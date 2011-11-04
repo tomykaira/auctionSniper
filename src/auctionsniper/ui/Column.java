dde377d1cca2995aad699fe234027c13ff478cd3
tomykaira <tomykaira@gmail.com>
2011-11-04 21:23:52 +0900

    get column's value in a oo-way

--- src/auctionsniper/ui/Column.java
+++ src/auctionsniper/ui/Column.java
@@ -1,12 +1,36 @@
 package auctionsniper.ui;
 
+import auctionsniper.SniperSnapshot;
+
 public enum Column {
-	ITEM_IDENTIFIER,
-	LAST_PRICE,
-	LAST_BID,
-	SNIPER_STATE;
+	ITEM_IDENTIFIER {
+		@Override
+		public Object valueIn(SniperSnapshot snapshot) {
+			return snapshot.itemId;
+		}
+	},
+	LAST_PRICE {
+		@Override
+		public Object valueIn(SniperSnapshot snapshot) {
+			return snapshot.lastPrice;
+		}
+	},
+	LAST_BID {
+		@Override
+		public Object valueIn(SniperSnapshot snapshot) {
+			return snapshot.lastBid;
+		}
+	},
+	SNIPER_STATE {
+		@Override
+		public Object valueIn(SniperSnapshot snapshot) {
+			return SnipersTableModel.textFor(snapshot.state);
+		}
+	};
 
 	public static Column at(int offset) {
 		return values()[offset];
 	}
+
+	abstract public Object valueIn(SniperSnapshot snapshot);
 }
