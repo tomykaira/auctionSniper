9b6a2331beb51e5adc374744918c406bc831c854
tomykaira <tomykaira@gmail.com>
2011-11-06 00:54:33 +0900

    integration test for MainWindow

---
+++ test/test/integration/auctionsniper/ui/MainWindowTest.java
@@ -0,0 +1,32 @@
+package test.integration.auctionsniper.ui;
+
+import static org.hamcrest.CoreMatchers.*;
+
+import org.junit.Test;
+
+import test.endtoend.auctionsniper.AuctionSniperDriver;
+import auctionsniper.UserRequestListener;
+import auctionsniper.ui.MainWindow;
+import auctionsniper.ui.SnipersTableModel;
+
+import com.objogate.wl.swing.probe.ValueMatcherProbe;
+
+public class MainWindowTest {
+	private final SnipersTableModel tableModel = new SnipersTableModel();
+	private final MainWindow mainWindow = new MainWindow(tableModel);
+	private final AuctionSniperDriver driver = new AuctionSniperDriver(100);
+
+	@Test public void
+	makeUserRequestWhenJoinButtonClicked() {
+		final ValueMatcherProbe<String> buttonProbe = new ValueMatcherProbe<String>(equalTo("an item-id"), "join request");
+		mainWindow.addUserRequestListener(new UserRequestListener() {
+
+			@Override
+			public void joinAuction(String itemId) {
+				buttonProbe.setReceivedValue(itemId);
+			}
+		});
+		driver.startBiddingFor("an item-id");
+		driver.check(buttonProbe);
+	}
+}
