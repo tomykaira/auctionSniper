73c5765df1e026ac3b4f38fbd0c3d0dde0c4738e
tomykaira <tomykaira@gmail.com>
2011-11-06 09:27:15 +0900

    pass UI joinAuction test

--- test/test/integration/auctionsniper/ui/MainWindowTest.java
+++ test/test/integration/auctionsniper/ui/MainWindowTest.java
@@ -14,19 +14,18 @@ import com.objogate.wl.swing.probe.ValueMatcherProbe;
 public class MainWindowTest {
 	private final SnipersTableModel tableModel = new SnipersTableModel();
 	private final MainWindow mainWindow = new MainWindow(tableModel);
-	private final AuctionSniperDriver driver = new AuctionSniperDriver(100);
+	private final AuctionSniperDriver driver = new AuctionSniperDriver(1000);
 
 	@Test public void
 	makeUserRequestWhenJoinButtonClicked() {
-		final ValueMatcherProbe<String> buttonProbe = new ValueMatcherProbe<String>(equalTo("an item-id"), "join request");
+		final ValueMatcherProbe<String> buttonProbe = new ValueMatcherProbe<String>(equalTo("item-id"), "join request");
 		mainWindow.addUserRequestListener(new UserRequestListener() {
-
 			@Override
 			public void joinAuction(String itemId) {
 				buttonProbe.setReceivedValue(itemId);
 			}
 		});
-		driver.startBiddingFor("an item-id");
+		driver.startBiddingFor("item-id");
 		driver.check(buttonProbe);
 	}
 }
