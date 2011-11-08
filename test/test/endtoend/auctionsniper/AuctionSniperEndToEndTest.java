bdc977370266ee2b2273b2288e541b2fdd3d22b1
tomykaira <tomykaira@gmail.com>
2011-11-08 13:08:42 +0900

    create sniperLosesAnAuctionWhenThePriceIsTooHigh

--- test/test/endtoend/auctionsniper/AuctionSniperEndToEndTest.java
+++ test/test/endtoend/auctionsniper/AuctionSniperEndToEndTest.java
@@ -77,7 +77,28 @@ public class AuctionSniperEndToEndTest {
 
 		application.showsSniperHasWonAcution(auction, 1098);
 		application.showsSniperHasWonAcution(auction2, 521);
-}
+	}
+
+	@Test
+	public void sniperLosesAnAuctionWhenThePriceIsTooHigh() throws Exception {
+		auction.startSellingItem();
+		application.startBiddingWithStopPrice(auction, 1100);
+		auction.hasReceivedJoinRequestFrom(ApplicationRunner.SNIPER_XMPP_ID);
+
+		auction.reportPrice(1000, 98, "other bidder");
+		application.hasShownSniperIsBidding(auction, 1000, 1098);
+
+		auction.hasReceivedBid(1098, ApplicationRunner.SNIPER_XMPP_ID);
+
+		auction.reportPrice(1197, 10, "third party");
+		application.hasShownSniperIsLosing(auction, 1197, 1098);
+
+		auction.reportPrice(1207, 10, "fourth party");
+		application.hasShownSniperIsLosing(auction, 1207, 1098);
+
+		auction.announceClosed();
+		application.showsSniperHasLostAcution(1000, 1098);
+	}
 
 	@After
 	public void stopAuction() {
