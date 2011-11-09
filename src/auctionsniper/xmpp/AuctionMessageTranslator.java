743ba5c771ada184b3d4abaf99f9b226952b3441
tomykaira <tomykaira@gmail.com>
2011-11-09 09:47:58 +0900

    pass failureReporter to Translator's constructor, test passes

--- src/auctionsniper/xmpp/AuctionMessageTranslator.java
+++ src/auctionsniper/xmpp/AuctionMessageTranslator.java
@@ -13,17 +13,21 @@ import auctionsniper.AuctionEventListener.PriceSource;
 public class AuctionMessageTranslator implements MessageListener {
 	private AuctionEventListener listener;
 	private final String sniperId;
+	private XMPPFailureReporter	failureReporter;
 
-	public AuctionMessageTranslator(String sniperId, AuctionEventListener listener) {
+	public AuctionMessageTranslator(String sniperId, AuctionEventListener listener, XMPPFailureReporter failureReporter) {
 		this.sniperId = sniperId;
 		this.listener = listener;
+		this.failureReporter = failureReporter;
 	}
 
 	@Override
 	public void processMessage(Chat chat, Message message) {
+		String body = message.getBody();
 		try {
-			translate(message.getBody());
+			translate(body);
 		} catch (Exception parseException) {
+			failureReporter.cannotTranslateMessage(sniperId, body, parseException);
 			listener.auctionFailed();
 		}
 	}
