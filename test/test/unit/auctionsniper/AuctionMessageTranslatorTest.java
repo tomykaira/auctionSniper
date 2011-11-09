4948eb9ff29837cdba24fbc724db2114be06550b
tomykaira <tomykaira@gmail.com>
2011-11-09 09:41:36 +0900

    assert that a failure log message is written in unitTest

--- test/test/unit/auctionsniper/AuctionMessageTranslatorTest.java
+++ test/test/unit/auctionsniper/AuctionMessageTranslatorTest.java
@@ -12,22 +12,23 @@ import test.endtoend.auctionsniper.ApplicationRunner;
 import auctionsniper.AuctionEventListener;
 import auctionsniper.AuctionEventListener.PriceSource;
 import auctionsniper.xmpp.AuctionMessageTranslator;
+import auctionsniper.xmpp.XMPPFailureReporter;
 
 @RunWith(JMock.class)
 public class AuctionMessageTranslatorTest {
 	public static final Chat UNUSED_CHAT = null;
 	private final Mockery context = new Mockery();
 	private final AuctionEventListener listener = context.mock(AuctionEventListener.class);
-	private final AuctionMessageTranslator translator = new AuctionMessageTranslator(ApplicationRunner.SNIPER_ID, listener);
+	private final XMPPFailureReporter	failureReporter = context.mock(XMPPFailureReporter.class);
+	private final AuctionMessageTranslator translator =
+			new AuctionMessageTranslator(ApplicationRunner.SNIPER_ID, listener, failureReporter);
 
 	@Test public void
 	notifiesAuctionClosedWhenCloseMessageReceived() {
 		context.checking(new Expectations() {{
 			oneOf(listener).auctionClosed();
 		}});
-		Message message = new Message();
-		message.setBody("SOLVersion: 1.1; Event: CLOSE;");
-		translator.processMessage(UNUSED_CHAT, message);
+		translator.processMessage(UNUSED_CHAT, message("SOLVersion: 1.1; Event: CLOSE;"));
 	}
 
 	@Test public void
@@ -36,9 +37,8 @@ public class AuctionMessageTranslatorTest {
 			exactly(1).of(listener).currentPrice(192, 7, PriceSource.FromOtherBidder);
 		}});
 
-		Message message = new Message();
-		message.setBody("SOLVersion: 1.1; Event: PRICE; CurrentPrice: 192; Increment: 7; Bidder: Someone else;");
-		translator.processMessage(UNUSED_CHAT, message);
+		translator.processMessage(UNUSED_CHAT,
+				message("SOLVersion: 1.1; Event: PRICE; CurrentPrice: 192; Increment: 7; Bidder: Someone else;"));
 	}
 
 	@Test public void
@@ -47,31 +47,38 @@ public class AuctionMessageTranslatorTest {
 			exactly(1).of(listener).currentPrice(234, 5, PriceSource.FromSniper);
 		}});
 
-		Message message = new Message();
-		message.setBody("SOLVersion: 1.1; Event: PRICE; CurrentPrice: 234; Increment: 5; Bidder: "+ ApplicationRunner.SNIPER_ID +";");
-		translator.processMessage(UNUSED_CHAT, message);
+		translator.processMessage(UNUSED_CHAT,
+				message("SOLVersion: 1.1; Event: PRICE; CurrentPrice: 234; Increment: 5; Bidder: "+ ApplicationRunner.SNIPER_ID +";"));
 	}
 
 	@Test public void
 	notifiesAuctionFailedWhenBadMessageReceived() {
-		context.checking(new Expectations() {{
-			exactly(1).of(listener).auctionFailed();
-		}});
-		Message message = new Message();
-		message.setBody("a bad message");
+		String badMessage = "a bad message";
+		expectFailureWithMessage(badMessage);
 
-		translator.processMessage(UNUSED_CHAT, message);
+		translator.processMessage(UNUSED_CHAT, message(badMessage));
 	}
 
 	@Test public void
 	notifiesAuctionFailedWhenEventTypeMissing() {
-		context.checking(new Expectations() {{
-			exactly(1).of(listener).auctionFailed();
-		}});
-		Message message = new Message();
-		message.setBody("SOLVersion: 1.1; CurrentPrice: 192; Increment: 7; Bidder: Someone else;");
+		String badMessage = "SOLVersion: 1.1; CurrentPrice: 192; Increment: 7; Bidder: Someone else;";
+		expectFailureWithMessage(badMessage);
 
-		translator.processMessage(UNUSED_CHAT, message);
+		translator.processMessage(UNUSED_CHAT, message(badMessage));
+	}
+
+	private void expectFailureWithMessage(final String badMessage) {
+		context.checking(new Expectations() {
+		{
+			oneOf(listener).auctionFailed();
+			oneOf(failureReporter).cannotTranslateMessage(with(ApplicationRunner.SNIPER_ID), with(badMessage), with(any(Exception.class)));
+		}});
+	}
+
+	private Message message(String body) {
+		Message message = new Message();
+		message.setBody(body);
+		return message;
 	}
 
 }
