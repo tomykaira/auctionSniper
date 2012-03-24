84fbc1a8866042a707177a4424b10248db11ab86
tomykaira <tomykaira@gmail.com>
2012-03-24 17:17:39 +0900

    ボタンが押された時に UserRequestListener に通知する(pass integration test)

--- src/auctionsniper/ui/MainWindow.java
+++ src/auctionsniper/ui/MainWindow.java
@@ -3,6 +3,8 @@ package auctionsniper.ui;
 import java.awt.BorderLayout;
 import java.awt.Container;
 import java.awt.FlowLayout;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 
 import javax.swing.JButton;
 import javax.swing.JFrame;
@@ -12,6 +14,7 @@ import javax.swing.JTable;
 import javax.swing.JTextField;
 
 import auctionsniper.UserRequestListener;
+import auctionsniper.util.Announcer;
 
 public class MainWindow extends JFrame {
 	public static final String MAIN_WINDOW_NAME = "Auction Sniper Main";
@@ -21,6 +24,7 @@ public class MainWindow extends JFrame {
 	public static final String	JOIN_BUTTON_NAME	= "Join Auction";
 
 	public final SnipersTableModel snipers;
+	private final Announcer<UserRequestListener> userRequests = Announcer.to(UserRequestListener.class);
 
 	public MainWindow(SnipersTableModel snipers) {
 		super(APPLICATION_TITLE);
@@ -40,6 +44,14 @@ public class MainWindow extends JFrame {
 
 		JButton joinAuctionButton = new JButton("Join Auction");
 		joinAuctionButton.setName(JOIN_BUTTON_NAME);
+		joinAuctionButton.addActionListener(new ActionListener(){
+
+			@Override
+			public void actionPerformed(ActionEvent e) {
+				userRequests.announce().joinAuction(itemIdField.getText());
+			}
+
+		});
 		controls.add(joinAuctionButton);
 		return controls;
 	}
@@ -58,6 +70,6 @@ public class MainWindow extends JFrame {
 	}
 
 	public void addUserRequestListener(UserRequestListener userRequestListener) {
-		// TODO Auto-generated method stub
+		userRequests.addListener(userRequestListener);
 	}
 }
