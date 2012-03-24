b385e8842506af0aaa6087a63d9d192abe6f75ed
tomykaira <tomykaira@gmail.com>
2012-03-24 16:33:29 +0900

    あたらしい入札を、UI を通じて開始するために end-to-end テストを変更

--- test/test/endtoend/auctionsniper/AuctionSniperDriver.java
+++ test/test/endtoend/auctionsniper/AuctionSniperDriver.java
@@ -3,14 +3,18 @@ package test.endtoend.auctionsniper;
 import static com.objogate.wl.swing.matcher.IterableComponentsMatcher.*;
 import static com.objogate.wl.swing.matcher.JLabelTextMatcher.*;
 
+import javax.swing.JButton;
+import javax.swing.JTextField;
 import javax.swing.table.JTableHeader;
 
 import auctionsniper.ui.MainWindow;
 
 import com.objogate.wl.swing.AWTEventQueueProber;
+import com.objogate.wl.swing.driver.JButtonDriver;
 import com.objogate.wl.swing.driver.JFrameDriver;
 import com.objogate.wl.swing.driver.JTableDriver;
 import com.objogate.wl.swing.driver.JTableHeaderDriver;
+import com.objogate.wl.swing.driver.JTextFieldDriver;
 import com.objogate.wl.swing.gesture.GesturePerformer;
 
 public class AuctionSniperDriver extends JFrameDriver {
@@ -34,5 +38,20 @@ public class AuctionSniperDriver extends JFrameDriver {
 		JTableHeaderDriver headers = new JTableHeaderDriver(this, JTableHeader.class);
 		headers.hasHeaders(matching(withLabelText("Item"), withLabelText("Last Price"),
 				withLabelText("Last Bid"), withLabelText("State")));
+	}
+
+	public void startBiddingFor(String itemId) {
+		itemIdField().replaceAllText(itemId);
+		bidButton().click();
+	}
+
+	private JTextFieldDriver itemIdField() {
+		JTextFieldDriver newItemId = new JTextFieldDriver(this, JTextField.class, named(MainWindow.NEW_ITEM_ID_NAME));
+		newItemId.focusWithMouse();
+		return newItemId;
+	}
+
+	private JButtonDriver bidButton() {
+		return new JButtonDriver(this, JButton.class, named(MainWindow.JOIN_BUTTON_NAME));
 	}
 }
