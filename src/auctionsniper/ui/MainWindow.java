a236d3ad0aa8395f6c91d5a3ebc2da3556179c77
tomykaira <tomykaira@gmail.com>
2012-03-24 16:43:46 +0900

    上部に Action Bar を追加

--- src/auctionsniper/ui/MainWindow.java
+++ src/auctionsniper/ui/MainWindow.java
@@ -2,10 +2,14 @@ package auctionsniper.ui;
 
 import java.awt.BorderLayout;
 import java.awt.Container;
+import java.awt.FlowLayout;
 
+import javax.swing.JButton;
 import javax.swing.JFrame;
+import javax.swing.JPanel;
 import javax.swing.JScrollPane;
 import javax.swing.JTable;
+import javax.swing.JTextField;
 
 public class MainWindow extends JFrame {
 	public static final String MAIN_WINDOW_NAME = "Auction Sniper Main";
@@ -20,14 +24,28 @@ public class MainWindow extends JFrame {
 		super(APPLICATION_TITLE);
 		setName(MAIN_WINDOW_NAME);
 		this.snipers = snipers;
-		fillContentPane(makeSnipersTable());
+		fillContentPane(makeSnipersTable(), makeControls());
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		setVisible(true);
 	}
 
-	private void fillContentPane(JTable snipersTable) {
+	private JPanel makeControls() {
+		JPanel controls = new JPanel(new FlowLayout());
+		final JTextField itemIdField = new JTextField();
+		itemIdField.setColumns(25);
+		itemIdField.setName(NEW_ITEM_ID_NAME);
+		controls.add(itemIdField);
+
+		JButton joinAuctionButton = new JButton("Join Auction");
+		joinAuctionButton.setName(JOIN_BUTTON_NAME);
+		controls.add(joinAuctionButton);
+		return controls;
+	}
+
+	private void fillContentPane(JTable snipersTable, JPanel controls) {
 		final Container contentPane = getContentPane();
 		contentPane.setLayout(new BorderLayout());
+		contentPane.add(controls, BorderLayout.NORTH);
 		contentPane.add(new JScrollPane(snipersTable), BorderLayout.CENTER);
 	}
 
