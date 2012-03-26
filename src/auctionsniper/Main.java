bb9fda3b0056f4ba0e7d9afd5b1c2c997cdb030b
tomykaira <tomykaira@gmail.com>
2012-03-26 12:49:55 +0900

    使用していなかった Item ID のインデックスを削除

--- src/auctionsniper/Main.java
+++ src/auctionsniper/Main.java
@@ -19,7 +19,6 @@ public class Main {
 	private static final int ARG_HOSTNAME = 0;
 	private static final int ARG_USERNAME = 1;
 	private static final int ARG_PASSWORD = 2;
-	private static final int ARG_ITEM_ID  = 3;
 
 	private final SnipersTableModel snipers = new SnipersTableModel();
 
