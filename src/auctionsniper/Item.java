9581b24bc3b824a2e4ef94ddf028edbc6d603d1d
tomykaira <tomykaira@gmail.com>
2012-03-27 11:48:58 +0900

    Create Item class

---
+++ src/auctionsniper/Item.java
@@ -0,0 +1,28 @@
+package auctionsniper;
+
+import org.apache.commons.lang.builder.EqualsBuilder;
+import org.apache.commons.lang.builder.HashCodeBuilder;
+import org.apache.commons.lang.builder.ToStringBuilder;
+
+public class Item {
+	public final String identifier;
+	public final int stopPrice;
+
+	public Item(String identifier, int stopPrice) {
+		this.identifier = identifier;
+		this.stopPrice = stopPrice;
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		return EqualsBuilder.reflectionEquals(this, obj);
+	}
+	@Override
+	public int hashCode() {
+		return HashCodeBuilder.reflectionHashCode(this);
+	}
+	@Override
+	public String toString() {
+		return ToStringBuilder.reflectionToString(this);
+	}
+}
