df53a710681971b8c7ad433f191f0e5d355c66f0
tomykaira <tomykaira@gmail.com>
2012-04-06 20:31:58 +0900

    failureReporter をコンストラクタに与え、失敗通知を投げさせる

--- test/test/unit/auctionsniper/AuctionMessageTranslatorTest.java
+++ test/test/unit/auctionsniper/AuctionMessageTranslatorTest.java
@@ -20,7 +20,8 @@ public class AuctionMessageTranslatorTest {
 	private final Mockery context = new Mockery();
 	private final AuctionEventListener listener = context.mock(AuctionEventListener.class);
 	private final XMPPFailureReporter failureReporter = context.mock(XMPPFailureReporter.class);
-	private final AuctionMessageTranslator translator = new AuctionMessageTranslator(ApplicationRunner.SNIPER_ID, listener);
+	private final AuctionMessageTranslator translator =
+			new AuctionMessageTranslator(ApplicationRunner.SNIPER_ID, listener, failureReporter);
 
 	@Test public void
 	notifiesAuctionClosedWhenCloseMessageReceived() {
