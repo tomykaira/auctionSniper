c46913f08b371dc65d5e4b08a656ec7ab82b1c52
tomykaira <tomykaira@gmail.com>
2012-04-08 00:34:17 +0900

    cannotTranslateMessage を呼びだした時に logger に通知する実装

--- src/auctionsniper/xmpp/LoggingXMPPFailureReporter.java
+++ src/auctionsniper/xmpp/LoggingXMPPFailureReporter.java
@@ -3,13 +3,18 @@ package auctionsniper.xmpp;
 import java.util.logging.Logger;
 
 public class LoggingXMPPFailureReporter implements XMPPFailureReporter {
+
+	final private Logger logger;
+
 	public LoggingXMPPFailureReporter(Logger logger) {
-		// TODO Auto-generated constructor stub
+		this.logger = logger;
 	}
 
 	@Override
 	public void cannotTranslateMessage(String auctionId, String failedMessage,
 			Exception exception) {
-
+		logger.severe("<" + auctionId + "> "
+				+ "Could not translate message \"" + failedMessage + "\" "
+				+ "because \"" + exception + "\"");
 	}
 }
