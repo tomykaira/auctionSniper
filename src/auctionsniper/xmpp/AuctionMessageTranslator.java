e71b3b1b84b10a9990cf582e7c71b9b1f9120910
tomykaira <tomykaira@gmail.com>
2012-08-31 12:03:09 +0900

    AuctionMessageTranslator を実装するための UnitTest

---
+++ src/auctionsniper/xmpp/AuctionMessageTranslator.java
@@ -0,0 +1,21 @@
+package auctionsniper.xmpp;
+
+import org.jivesoftware.smack.Chat;
+import org.jivesoftware.smack.MessageListener;
+import org.jivesoftware.smack.packet.Message;
+
+import auctionsniper.AuctionEventListener;
+
+public class AuctionMessageTranslator implements MessageListener {
+
+	public AuctionMessageTranslator(AuctionEventListener listener) {
+		// TODO Auto-generated constructor stub
+	}
+
+	@Override
+	public void processMessage(Chat arg0, Message arg1) {
+		// TODO Auto-generated method stub
+		
+	}
+
+}
---
+++ test/test/unit/auctionsniper/AuctionMessageTranslatorTest.java
@@ -0,0 +1,31 @@
+package test.unit.auctionsniper;
+
+import org.jivesoftware.smack.Chat;
+import org.jivesoftware.smack.packet.Message;
+import org.jmock.Expectations;
+import org.jmock.Mockery;
+import org.jmock.integration.junit4.JMock;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+
+import auctionsniper.AuctionEventListener;
+import auctionsniper.xmpp.AuctionMessageTranslator;
+
+@RunWith(JMock.class)
+public class AuctionMessageTranslatorTest {
+	public static final Chat UNUSED_CHAT = null;
+	private final Mockery context = new Mockery();
+	private final AuctionEventListener listener = context.mock(AuctionEventListener.class);
+	private final AuctionMessageTranslator translator = new AuctionMessageTranslator(listener);
+
+	@Test public void
+	notifiesAuctionClosedWhenCloseMessageReceived() {
+		context.checking(new Expectations() {{
+			oneOf(listener).auctionClosed();
+		}});
+		Message message = new Message();
+		message.setBody("SOLVersion: 1.1; Event: CLOSE;");
+		translator.processMessage(UNUSED_CHAT, message);
+	}
+
+}
