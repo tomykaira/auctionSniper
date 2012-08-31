d932e1574f701b50f2282e51dc35ae8cfa49eacf
tomykaira <tomykaira@gmail.com>
2012-08-31 14:44:05 +0900

    単一のセルを持つテーブルを実装
    end-to-end テストがグリーンにもどる

---
+++ src/auctionsniper/ui/SnipersTableModel.java
@@ -0,0 +1,19 @@
+package auctionsniper.ui;
+
+import javax.swing.table.AbstractTableModel;
+
+public class SnipersTableModel extends AbstractTableModel {
+	private String statusText = MainWindow.STATUS_JOINING;
+
+	@Override
+	public int getColumnCount() { return 1; }
+	@Override
+	public int getRowCount() { return 1; }
+	@Override
+	public Object getValueAt(int rowIndex, int columnIndex) { return statusText; }
+
+	public void setStatusText(String newStatusText) {
+		statusText = newStatusText;
+		fireTableRowsUpdated(0, 0);
+	}
+}
