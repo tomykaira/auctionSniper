3f5d016b46bb5cd87b7fa6b2d0bfb6c7cfab3d25
tomykaira <tomykaira@gmail.com>
2012-08-31 14:54:20 +0900

    価格表示を確認するメソッドを追加

--- test/test/endtoend/auctionsniper/ApplicationRunner.java
+++ test/test/endtoend/auctionsniper/ApplicationRunner.java
@@ -9,9 +9,11 @@ public class ApplicationRunner {
 	public static final String SNIPER_PASSWORD = "sniper";
 	public static final String SNIPER_XMPP_ID = SNIPER_ID + "@" + XMPP_HOSTNAME + "/Auction";
 	private AuctionSniperDriver driver;
+	private String itemId;
 
 	// trigger an event to drive the test
 	public void startBiddingIn(final FakeAuctionServer auction) {
+		itemId = auction.getItemId();
 		Thread thread = new Thread("Test Application") {
 			@Override public void run() {
 				try {
@@ -47,5 +49,17 @@ public class ApplicationRunner {
 
 	public void hasShownSniperIsWinning() {
 		driver.showsSniperStatus(MainWindow.STATUS_WINNING); // test case
+	}
+
+	public void hasShownSniperIsBidding(int lastPrice, int lastBid) {
+		driver.showsSniperStatus(itemId, lastPrice, lastBid, MainWindow.STATUS_BIDDING);
+	}
+
+	public void hasShownSniperIsWinning(int lastPrice) {
+		driver.showsSniperStatus(itemId, lastPrice, lastPrice, MainWindow.STATUS_WINNING);
+	}
+
+	public void showsSniperHasWonAcution(int lastPrice) {
+		driver.showsSniperStatus(itemId, lastPrice, lastPrice, MainWindow.STATUS_WON);
 	}
 }
--- test/test/endtoend/auctionsniper/AuctionSniperDriver.java
+++ test/test/endtoend/auctionsniper/AuctionSniperDriver.java
@@ -1,7 +1,9 @@
 package test.endtoend.auctionsniper;
 
 import static com.objogate.wl.swing.matcher.JLabelTextMatcher.*;
+import static com.objogate.wl.swing.matcher.IterableComponentsMatcher.*;
 import static org.hamcrest.CoreMatchers.*;
+import static java.lang.String.valueOf;
 import auctionsniper.ui.MainWindow;
 
 import com.objogate.wl.swing.AWTEventQueueProber;
@@ -18,5 +20,13 @@ public class AuctionSniperDriver extends JFrameDriver {
 
 	public void showsSniperStatus(String statusText) {
 		new JTableDriver(this).hasCell(withLabelText(equalTo(statusText)));
+	}
+
+	public void showsSniperStatus(String itemId, int lastPrice, int lastBid,
+			String statusText) {
+		JTableDriver table = new JTableDriver(this);
+		table.hasRow(
+				matching(withLabelText(itemId), withLabelText(valueOf(lastPrice)),
+						withLabelText(valueOf(lastBid)), withLabelText(statusText)));
 	}
 }
