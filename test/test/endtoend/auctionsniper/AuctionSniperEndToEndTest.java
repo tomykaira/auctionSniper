10da3fbaa3b7d7781bf160a1d273bcf94f4ddb2f
tomykaira <tomykaira@gmail.com>
2012-08-31 13:42:47 +0900

    入札して落札成功するシナリオの end-to-end テストを作成

--- test/test/endtoend/auctionsniper/AuctionSniperEndToEndTest.java
+++ test/test/endtoend/auctionsniper/AuctionSniperEndToEndTest.java
@@ -32,6 +32,24 @@ public class AuctionSniperEndToEndTest {
 		application.showsSniperHasLostAcution();
 	}
 
+	@Test
+	public void sniperWinsAnAuctionByBiddingHigher() throws Exception {
+		auction.startSellingItem();
+		application.startBiddingIn(auction);
+		auction.hasReceivedJoinRequestFrom(ApplicationRunner.SNIPER_XMPP_ID);
+
+		auction.reportPrice(1000, 98, "other bidder");
+		application.hasShownSniperIsBidding();
+
+		auction.hasReceivedBid(1098, ApplicationRunner.SNIPER_XMPP_ID);
+
+		auction.reportPrice(1098, 97, ApplicationRunner.SNIPER_XMPP_ID);
+		application.hasShownSniperIsWinning();
+
+		auction.announceClosed();
+		application.showsSniperHasWonAcution();
+	}
+
 	@After
 	public void stopAuction() {
 		auction.stop();
