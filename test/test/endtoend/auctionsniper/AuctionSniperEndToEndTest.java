5bcdd1a6efeff129bf6ba1cec0bbfd656a56782c
tomykaira <tomykaira@gmail.com>
2012-08-31 11:29:02 +0900

    end-to-end テストに sniperMakesAHigherBidButLoses 追加

--- test/test/endtoend/auctionsniper/AuctionSniperEndToEndTest.java
+++ test/test/endtoend/auctionsniper/AuctionSniperEndToEndTest.java
@@ -12,7 +12,22 @@ public class AuctionSniperEndToEndTest {
 	public void sniperJoinsAuctionUntilAuctionCloses() throws Exception {
 		auction.startSellingItem();
 		application.startBiddingIn(auction);
-		auction.hasReceivedJoinRequestFromSniper();
+		auction.hasReceivedJoinRequestFrom(ApplicationRunner.SNIPER_XMPP_ID);
+		auction.announceClosed();
+		application.showsSniperHasLotAcution();
+	}
+
+	@Test
+	public void sniperMakesAHigherBidButLoses() throws Exception {
+		auction.startSellingItem();
+		application.startBiddingIn(auction);
+		auction.hasReceivedJoinRequestFrom(ApplicationRunner.SNIPER_XMPP_ID);
+
+		auction.reportPrice(1000, 98, "other bidder");
+		application.hasShownSniperIsBidding();
+
+		auction.hasReceivedBid(1098, ApplicationRunner.SNIPER_XMPP_ID);
+
 		auction.announceClosed();
 		application.showsSniperHasLotAcution();
 	}
